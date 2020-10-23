1b38adb25e228e2fa54307d04315cd76ef5e8481
Tarek Ali <tarekali15@outlook.com>
2020-10-23 18:33:26 -0500

    webcam

---
+++ app/scripts/video.js
@@ -0,0 +1,12 @@
+const video = document.getElementsByClassName("camera");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.log(`Error: ${error}`);
+    });
+}
