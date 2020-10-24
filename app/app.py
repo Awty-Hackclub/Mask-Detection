8924bb81f237d9e987988adabc7eae207cebe787
Tarek Ali <tarekali15@outlook.com>
2020-10-23 19:55:19 -0500

    json dumping of results

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from model import detect_mask_image as detect
+from json import dumps 
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ def main():
 def api():
     image = request.files['image']
     is_masked = detect.detect_and_predict_mask(image)
-
+    return dumps({"isMasked": is_masked}) 
 
 if __name__ == '__main__':
     app.run()
