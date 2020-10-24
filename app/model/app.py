75f92ce80315f00751b1b215e65031d8b3bdb770
gallo-json <gallojsantiago@gmail.com>
2020-10-24 09:34:53 -0500

    remove annoying comments

--- app/model/app.py
+++ app/model/app.py
@@ -16,10 +16,7 @@ import imutils
 
 outputFrame = None
 lock = threading.Lock()
-# initialize a flask object
 app = Flask(__name__, template_folder="templates")
-# initialize the video stream and allow the camera sensor to
-# warmup
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
@@ -37,109 +34,63 @@ weightsPath = os.path.sep.join(
 )
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
 print("[INFO] loading face mask detector model...")
 maskNet = load_model("mask_detector.model")
 
 
 def detect_and_predict_mask(frame):
-
-    # grab the dimensions of the frame and then construct a blob
-    # from it
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-    # pass the blob through the network and obtain the face detections
     faceNet.setInput(blob)
     detections = faceNet.forward()
 
-    # initialize our list of faces, their corresponding locations,
-    # and the list of predictions from our face mask network
     faces = []
     locs = []
     preds = []
 
-    # loop over the detections
     for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the detection
         confidence = detections[0, 0, i, 2]
 
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
         if confidence > 0.5:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the object
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
 
-            # ensure the bounding boxes fall within the dimensions of
-            # the frame
             (startX, startY) = (max(0, startX), max(0, startY))
             (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-            # extract the face ROI, convert it from BGR to RGB channel
-            # ordering, resize it to 224x224, and preprocess it
             face = frame[startY:endY, startX:endX]
             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
             face = cv2.resize(face, (224, 224))
             face = img_to_array(face)
             face = preprocess_input(face)
 
-            # add the face and bounding boxes to their respective
-            # lists
             faces.append(face)
             locs.append((startX, startY, endX, endY))
 
-    # only make a predictions if at least one face was detected
     if len(faces) > 0:
-        # for faster inference we'll make batch predictions on *all*
-        # faces at the same time rather than one-by-one predictions
-        # in the above `for` loop
         faces = np.array(faces, dtype="float32")
         preds = maskNet.predict(faces, batch_size=32)
 
-    # return a 2-tuple of the face locations and their corresponding
-    # locations
     return (locs, preds)
-
-# load our serialized face detector model from disk
-
-# initialize the video stream and allow the camera sensor to warm up
-
-# loop over the frames from the video stream
-
 
 def detect():
     global vs, outputFrame, lock
     while True:
-
-        # grab the frame from the threaded video stream and resize it
-        # to have a maximum width of 400 pixels
         frame = vs.read()
         frame = imutils.resize(frame, width=400)
 
-        # detect faces in the frame and determine if they are wearing a
-        # face mask or not
         (locs, preds) = detect_and_predict_mask(frame)
 
-        # loop over the detected face locations and their corresponding
-        # locations
         for (box, pred) in zip(locs, preds):
-            # unpack the bounding box and predictions
             (startX, startY, endX, endY) = box
             (mask, withoutMask) = pred
 
-            # determine the class label and color we'll use to draw
-            # the bounding box and text
             label = "Mask" if mask > withoutMask else "No Mask"
             color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-            # include the probability in the label
             label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-            # display the label and bounding box rectangle on the output
-            # frame
             cv2.putText(
                 frame,
                 label,
@@ -157,34 +108,25 @@ def detect():
 
 def generate():
     global outputFrame, lock
-    # grab global references to the output frame and lock variables
-    # loop over frames from the output stream
+
     while True:
-        # wait until the lock is acquired
         with lock:
-            # check if the output frame is available, otherwise skip
-            # the iteration of the loop
             if outputFrame is None:
                 continue
-            # encode the frame in JPEG format
+           
             (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-            # ensure the frame was successfully encoded
+            
             if not flag:
                 continue
-        # yield the output frame in the byte format
+        
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
               bytearray(encodedImage) + b'\r\n')
 
-
 @app.route("/video_feed")
 def video_feed():
-    # return the response generated along with the specific media
-    # type (mime type)
     return Response(generate(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
-
-# check to see if this is the main thread of execution
 if __name__ == '__main__':
     t = threading.Thread(target=detect)
     t.daemon = True
