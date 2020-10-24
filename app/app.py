30c0944b5991456b2039b71376b76c95636c6efd
Tarek Ali <tarekali15@outlook.com>
2020-10-23 20:31:56 -0500

    stripped down html

--- app/app.py
+++ app/app.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template, request
 from model import detect_mask_image as detect
-from json import dumps 
+
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-
-@app.route('/api')
-def api():
-    image = request.files['image']
-    is_masked = detect.detect_and_predict_mask(image)
-    return dumps({"isMasked": is_masked}) 
 
 if __name__ == '__main__':
     app.run()
